8ef6bf9892828f5a31edf664eb84e5b58905db66
MaxBasev <maxbasev@gmail.com>
2021-08-11 20:55:45 +0700

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -123,6 +123,14 @@ module.exports = {
     },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-gatsby-cloud`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
